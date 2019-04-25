3206954a564e71e2b92a8e9ffae963b828e9978c
Nafeesa <nafyxnaz@gmail.com>
2019-04-25 16:36:52 +0100

    Second Commit

---
+++ src/UI.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nafy
+ */
+
+import java.util.ArrayList;
+import processing.core.PApplet;
+
+public class UI extends PApplet
+{   
+    boolean[] keys = new boolean[1024];
+
+    public void keyPressed()
+    {
+        keys[keyCode] = true;
+    }
+    
+    public void keyReleased()
+    {
+        keys[keyCode] = false;
+    }
+
+    public boolean checkKey(int c)
+    {
+        return keys[c] || keys[Character.toUpperCase(c)];
+    }
+    
+
+    public void settings()
+    {
+        size(800, 800);
+        // Use fullscreen instead of size to make your interface fullscreen
+        //fullScreen(P3D); 
+    }
+
+
+    public void draw()
+    {
+        
+        if (checkKey(LEFT))
+        {
+            System.out.println("Left arrow key pressed");
+        }
+    }
+}
