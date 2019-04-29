15192c5d67dcedd1caeef2634be3ccf34248d2e1
Nafeesa <nafyxnaz@gmail.com>
2019-04-29 22:44:48 +0100

    another commit

--- src/HUD.java
+++ src/HUD.java
@@ -44,14 +44,14 @@ public class HUD extends GameObject{
         //loop to add 4 buttons
         for(i = 0; i < buttonAmount; i++){ 
             for( j = 0; j < buttonAmount; j++){
-                buttons.add(new Button( borderW / 10 , (buttonInterval) + buttonInterval * j , buttonWidth));
+                 buttons.add(new Button( borderW / 10  , (buttonInterval) + buttonInterval * j ,  buttonWidth));
             };
             buttons.add(new Button( borderW / 10  , (buttonInterval) + buttonInterval * i ,  buttonWidth));
         };
 
        // loop to add in the bar meters
         for( i = 0; i < barAmount; i ++){ 
-            bars.add(new Bar(borderW / 4 + (borderW / 4 ) * i, borderH - (borderH / 4), -50 , random(-100, -10 * i) ));
+            bars.add(new Bar(borderW / 4 + (borderW / 4 ) * i, borderH - (borderH / 4), -50 ,random(-100, -10 * i) ));
         }
 
         //creates 
@@ -60,106 +60,106 @@ public class HUD extends GameObject{
     }
 
     public void render(){
-     background(0);
-     fill(255);
+     ui.background(0);
+     ui.fill(255);
      //Covers the top white.
-     rect(0,0, width , borderH);
+     ui.rect(0,0, ui.width , borderH);
 
      //creates the top border and bottom border layout
-     fill(0,128,200);
-     stroke(0);
-     pushMatrix();
-     translate(borderW , 0);
-     rect(0 , 0, width / 2 , borderH / 2);
-     translate(0 , height - borderW / 2);
-     fill(0,128,200);
-     rect(0, 0 , width / 2 , borderH);
-     fill(255);
-     rect(0 , 0, width / 2 , borderH / 4);
-     stroke(0);
-     popMatrix();
+     ui.fill(0,128,200);
+     ui.stroke(0);
+     ui.pushMatrix();
+     ui.translate(borderW , 0);
+     ui.rect(0 , 0, ui.width / 2 , borderH / 2);
+     ui.translate(0 , ui.height - borderW / 2);
+     ui.fill(0,128,200);
+     ui.rect(0, 0 , ui.width / 2 , borderH);
+     ui.fill(255);
+     ui.rect(0 , 0, ui.width / 2 , borderH / 4);
+     ui.stroke(0);
+     ui.popMatrix();
 
      //creates the left side layout   
-     fill(0,128,200);
-     rect(0 , borderH , borderW , height /2); 
-     triangle(0 , borderH / 2 , 0 , borderH , borderW , borderH);
-     triangle(0 , 0 , borderW , 0 , borderW , borderH / 2);
-     quad(0 , (height - borderH ) + (borderH / 4) , borderW , (height - borderH ) + (borderH /4) , borderW + (borderW / 4) , height, 0 , height); 
-     fill(255);
-     stroke(0);
-     rect(0, height - borderH, borderW + (borderW / 12) ,borderH / 4);
-     quad(borderW + (borderW / 12) , height - borderH , borderW, height - (borderH - borderH / 4) , borderW + (borderW / 4) , height , (borderW + (borderW / 4)) + borderW / 8 , height); 
-     fill(255);
-     stroke(0);
+     ui.fill(0,128,200);
+     ui.rect(0 , borderH , borderW , ui.height /2); 
+     ui.triangle(0 , borderH / 2 , 0 , borderH , borderW , borderH);
+     ui.triangle(0 , 0 , borderW , 0 , borderW , borderH / 2);
+     ui.quad(0 , (ui.height - borderH ) + (borderH / 4) , borderW , (ui.height - borderH ) + (borderH /4) , borderW + (borderW / 4) , ui.height, 0 , ui.height); 
+     ui.fill(255);
+     ui.stroke(0);
+     ui.rect(0, ui.height - borderH, borderW + (borderW / 12) ,borderH / 4);
+     ui.quad(borderW + (borderW / 12) , ui.height - borderH , borderW, ui.height - (borderH - borderH / 4) , borderW + (borderW / 4) , ui.height , (borderW + (borderW / 4)) + borderW / 8 , ui.height); 
+     ui.fill(255);
+     ui.stroke(0);
 
      //creates the right side layout
-     pushMatrix();
-     translate(width - borderW , 0);
-     fill(0,128,200);
-     rect(0 , borderH , borderW , height /2);
-     quad(0 , (height - borderH ) + (borderH / 4) , borderW , (height - borderH ) + (borderH /4) ,  borderW  , height, -(borderW / 4) , height); 
-     triangle(borderW , borderH / 2 , 0 , borderH , borderW , borderH); 
-     triangle(0 , 0 , borderW , 0 , 0 , borderH / 2);
-     fill(255);
-     stroke(0);
-     rect(-(borderW/12), height - borderH, borderW + (borderW /12) ,borderH / 4);
-     quad(-(borderW / 12) , height - borderH , 0, height - (borderH - borderH / 4) ,  - (borderW /4 ) , height ,  - (borderW / 4) - borderW / 8 , height); 
-     fill(255);
-     popMatrix();
+     ui.pushMatrix();
+     ui.translate(ui.width - borderW , 0);
+     ui.fill(0,128,200);
+     ui.rect(0 , borderH , borderW , ui.height /2);
+     ui.quad(0 , (ui.height - borderH ) + (borderH / 4) , borderW , (ui.height - borderH ) + (borderH /4) ,  borderW  , ui.height, -(borderW / 4) , ui.height); 
+     ui.triangle(borderW , borderH / 2 , 0 , borderH , borderW , borderH); 
+     ui.triangle(0 , 0 , borderW , 0 , 0 , borderH / 2);
+     ui.fill(255);
+     ui.stroke(0);
+     ui.rect(-(borderW/12), ui.height - borderH, borderW + (borderW /12) ,borderH / 4);
+     ui.quad(-(borderW / 12) , ui.height - borderH , 0, ui.height - (borderH - borderH / 4) ,  - (borderW /4 ) , ui.height ,  - (borderW / 4) - borderW / 8 , ui.height); 
+     ui.fill(255);
+     ui.popMatrix();
 
 
      //buttons for bttom left
-     strokeWeight(5);
+     ui.strokeWeight(5);
      for ( i = 0; i  < buttonAmount; i++){
-     pushMatrix();
-     translate(0,height - borderH);  
+     ui.pushMatrix();
+     ui.translate(0,ui.height - borderH);  
        Button button = buttons.get(i);
        //button.update
        button.render();
        if (i == 0) {    
-        pushMatrix();
-        translate(borderW / 10, buttonInterval);
-        fill(255);
-        textSize(20);
-        text("Radar", borderW / 4  , buttonInterval / 3);
-        popMatrix();
+        ui.pushMatrix();
+        ui.translate(borderW / 10, buttonInterval);
+        ui.fill(255);
+        ui.textSize(20);
+        ui.text("Radar", borderW / 4  , buttonInterval / 3);
+        ui.popMatrix();
        }
        else if (i == 1){
-        pushMatrix();
-        translate(borderW / 10, buttonInterval);
-        fill(255);
-        textSize(20);
-        text("Map", borderW / 4  , buttonInterval + buttonInterval / 3);
-        popMatrix();
+        ui.pushMatrix();
+        ui.translate(borderW / 10, buttonInterval);
+        ui.fill(255);
+        ui.textSize(20);
+        ui.text("Map", borderW / 4  , buttonInterval + buttonInterval / 3);
+        ui.popMatrix();
 
        }
-      popMatrix(); 
+      ui.popMatrix(); 
      }
        //buttoms for bottom right 
        for ( j = 0; j < buttonAmount; j++){
-       pushMatrix();
-       translate(width - (borderW - borderW /10), height- borderH);
+       ui.pushMatrix();
+       ui.translate(ui.width - (borderW - borderW /10), ui.height- borderH);
        Button button = buttons.get(j);
        //button.update();
        button.render();
 
         if (j == 0) {    
-          pushMatrix();
-          translate(borderW / 10, buttonInterval);
-          fill(255);
-          textSize(20);
-          text("Ship Status", borderW / 4  , buttonInterval / 3);
-          popMatrix();
+          ui.pushMatrix();
+          ui.translate(borderW / 10, buttonInterval);
+          ui.fill(255);
+          ui.textSize(20);
+          ui.text("Ship Status", borderW / 4  , buttonInterval / 3);
+          ui.popMatrix();
        }
          else if (j == 1){
-            pushMatrix();
-            translate(borderW / 10, buttonInterval);
-            fill(255);
-            textSize(20);
-            text("Warpdrive", borderW / 4  , buttonInterval + buttonInterval / 3);
-            popMatrix();
+            ui.pushMatrix();
+            ui.translate(borderW / 10, buttonInterval);
+            ui.fill(255);
+            ui.textSize(20);
+            ui.text("Warpdrive", borderW / 4  , buttonInterval + buttonInterval / 3);
+            ui.popMatrix();
        };
-       popMatrix();
+       ui.popMatrix();
      }
 
      //renders and updates the rader
@@ -167,38 +167,38 @@ public class HUD extends GameObject{
      r.update();
 
      //map
-     if ( click == true  ){
-       pushMatrix();
-       translate(borderW,borderH);
+     if ( ui.click == true  ){
+       ui.pushMatrix();
+       ui.translate(borderW,borderH);
        m.render();
-       popMatrix();
+       ui.popMatrix();
      }
 
      //time
-     pushMatrix();
-     translate(width / 2, height / 2);
+     ui.pushMatrix();
+     ui.translate(ui.width / 2, ui.height / 2);
      t.render();
-     popMatrix();
+     ui.popMatrix();
 
 
-     pushMatrix();
-     translate(width - borderW , height / 2);
+     ui.pushMatrix();
+     ui.translate(ui.width - borderW , ui.height / 2);
      //adding bar meters
      for(i = 0; i < barAmount; i++){
        Bar bar = bars.get(i);     
-       fill(255  , 0 , 0 );     
+       ui.fill(255  , 0 , 0 );     
        bar.update();
        bar.render();   
        if(i == 0){
-       textSize(20);
-       text("Ship HP" , borderW /4 + (borderW /4  * i) , borderH - borderH/ 6);
+       ui.textSize(20);
+       ui.text("Ship HP" , borderW /4 + (borderW /4  * i) , borderH - borderH/ 6);
        }  
        else if( i == 1){
-       text("Pilot HP" , borderW /4 + (borderW /4  * i)  ,borderH - borderH/ 6);
+       ui.text("Pilot HP" , borderW /4 + (borderW /4  * i)  ,borderH - borderH/ 6);
        }
        else if ( i == 2){
 
-       text("Enemy HP" , borderW /4 + (borderW /4  * i)  , borderH - borderH/ 6);  
+       ui.text("Enemy HP" , borderW /4 + (borderW /4  * i)  , borderH - borderH/ 6);  
        }
    }
 
@@ -206,7 +206,7 @@ public class HUD extends GameObject{
 
 
 
-     popMatrix();
+     ui.popMatrix();
 
 
    }
