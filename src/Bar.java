ec25af3fcd3f0a47e05efd8255dd36cd99dca3a3
Nafeesa <nafyxnaz@gmail.com>
2019-05-01 00:35:18 +0100

    last day

--- src/Bar.java
+++ src/Bar.java
@@ -12,43 +12,43 @@
 public class Bar  {
   
    
- float x;        
- float y;
- float barW;
- float barH;
- float random;
- float time;
- float bMax;
- float bMin;
- UI ui;
- 
-   private int change = 0;
-  
+            float x;        
+            float y;
+            float barW;
+            float barH;
+            float random;
+            float time;
+            float bMax;
+            float bMin;
+            UI ui;
+
+                private int change = 0;
+
   public Bar (UI ui, float x , float y , float barW) {
-    this.ui = ui;
-   
-    this.x = x;
-    this.y = y;
-    this.barW = barW;
-    this.barH = -ui.random(30, 70);
-    this.time = 60;
-    this.bMax = barH - 50;
-    this.bMin = 0;
-    this.change = 1;
+            this.ui = ui;
+
+            this.x = x;
+            this.y = y;
+            this.barW = barW;
+            this.barH = -ui.random(30, 70);
+            this.time = 60;
+            this.bMax = barH - 50;
+            this.bMin = 0;
+            this.change = 1;
   }
   
   
    public void render() { 
-    ui.rect(x , y , barW  ,barH);
-      barH += change;
+            ui.rect(x , y , barW  ,barH);
+                    barH += change;
   }
   
   
   
   public void update(){
-    if(barH > bMin || barH < bMax )
-    {
-      change*=-1;
-    }
+                if(barH > bMin || barH < bMax )
+                        {
+                          change*=-1;
+                        }
   }
 }
