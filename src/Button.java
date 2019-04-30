b13f433d82806f4bca8474fe70aa40061a73cd4d
Nafeesa <nafyxnaz@gmail.com>
2019-04-30 18:19:13 +0100

    last day

--- src/Button.java
+++ src/Button.java
@@ -1,4 +1,5 @@
 
+
 import processing.core.PVector;
 
 /*
@@ -30,6 +31,52 @@ public class Button extends GameObject
     public void render()
     {
         ui.noFill();
+        ui.stroke(255, 0, 0);
+        ui.rect(pos.x, pos.y, width, height);
+        ui.textAlign(UI.CENTER, ui.CENTER);
+        ui.text(text, pos.x + width * 0.5f, pos.y + height * 0.5f);
+    }
+    
+    @Override
+    public void update(){
+        
+    }
+}
+
+/*
+import processing.core.PVector;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nafy
+ */
+
+/*
+public class Button extends GameObject
+{
+    private float width;
+    private float height;
+    private String text;
+    private UI ui;
+
+    public Button(UI ui, float x, float y, float width, float height, String text)
+    {
+        this.ui = ui;
+        pos = new PVector(x, y);
+        this.width = width;
+        this.height = height;
+        this.text = text;
+    }
+
+    public void render()
+    {
+        ui.noFill();
         ui.stroke(255);
         ui.rect(pos.x, pos.y, width, height);
         ui.textAlign(UI.CENTER, ui.CENTER);
@@ -41,3 +88,4 @@ public class Button extends GameObject
         
     }
 }
+*/
